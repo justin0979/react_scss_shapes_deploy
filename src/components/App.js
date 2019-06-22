596cc4cac05a98b3abada3070981fcbc4eeb9762
justin <justin0979@gmail.com>
2019-06-22 16:09:14 -0500

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../sass/main.scss';
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="triangle">triangle</div>
+    </div>
+  );
+};
+
+export default App;
